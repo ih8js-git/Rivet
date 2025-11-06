5aa37f3d254ea875676f1f8957b17a29606e5758
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-06 17:32:12 +0100

    [ADD] src/api/message/create_message.rs

---
+++ src/api/message/create_message.rs
@@ -0,0 +1,31 @@
+use reqwest::{Client, Response};
+use serde_json::json;
+
+use crate::Error;
+
+pub async fn create_message(
+    client: &Client,
+    channel_id: &str,
+    token: &str,
+    content: Option<String>,
+    tts: bool,
+) -> Result<Response, Error> {
+    let api_url = format!("https://discord.com/api/v10/channels/{channel_id}/messages?");
+
+    let content: &str = &content.unwrap_or("".to_string());
+
+    let payload = json!({
+        "content": content,
+        "tts": tts,
+    });
+
+    let response = client
+        .post(&api_url)
+        .header("Authorization", token)
+        .header("Content-Type", "application/json")
+        .json(&payload)
+        .send()
+        .await?;
+
+    Ok(response)
+}

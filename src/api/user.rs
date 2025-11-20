052387e0609f7cab02352ed403a7197b00f5949a
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-20 14:40:49 +0100

    [UPDATE] user.rs
    Removed impl from User

--- src/api/user.rs
+++ src/api/user.rs
@@ -1,10 +1,5 @@
 use serde::Deserialize;
 
-use crate::{
-    Error,
-    api::{ApiClient, Guild},
-};
-
 #[derive(Debug, Deserialize, Clone)]
 pub struct User {
     pub id: String,
@@ -13,26 +8,4 @@ pub struct User {
     pub global_name: Option<String>,
     //pub avatar : Option<String>,
     //pub bot: Option<bool>,
-}
-
-impl User {
-    pub async fn get_guilds(api_client: &ApiClient) -> Result<Vec<Guild>, Error> {
-        let url = format!("{}/users/@me/guilds", api_client.base_url);
-        let response = api_client
-            .http_client
-            .get(url)
-            .header("Authorization", &api_client.auth_token)
-            .send()
-            .await
-            .map_err(|e| format!("API Error: {e}"))?;
-
-        if response.status().is_success() {
-            response
-                .json::<Vec<Guild>>()
-                .await
-                .map_err(|e| format!("JSON Error: {e}").into())
-        } else {
-            Err(format!("API Error: {}", response.status()).into())
-        }
-    }
 }

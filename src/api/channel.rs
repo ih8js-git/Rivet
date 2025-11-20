f0f436e3394aa0aa91cf72db5e95752e03ddf7d0
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-20 14:38:54 +0100

    [UPDATE] channel.rs
    Removed impl for Channel

--- src/api/channel.rs
+++ src/api/channel.rs
@@ -1,7 +1,5 @@
 use serde::Deserialize;
 
-use crate::{Error, api::ApiClient};
-
 #[derive(Debug, Deserialize, Clone)]
 pub struct Channel {
     pub id: String,
@@ -9,31 +7,4 @@ pub struct Channel {
     #[serde(rename = "type")]
     pub channel_type: u8,
     pub guild_id: Option<String>,
-}
-
-impl Channel {
-    pub async fn from_id(api_client: &ApiClient, channel_id: &str) -> Result<Self, Error> {
-        let url = format!("{}/channels/{channel_id}", api_client.base_url);
-        let response = api_client
-            .http_client
-            .get(&url)
-            .header("Authorization", &api_client.auth_token)
-            .send()
-            .await?;
-
-        if !response.status().is_success() {
-            let status = response.status();
-            let body = response
-                .text()
-                .await
-                .unwrap_or_else(|_| "Failed to read error body".to_string());
-
-            return Err(format!("API Error: Status {status}. Details: {body}").into());
-        }
-
-        Ok(response
-            .json::<Self>()
-            .await
-            .map_err(|e| format!("JSON Decoding Error: {e}."))?)
-    }
 }
